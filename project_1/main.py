b0fb947e31a1dbe0a76751af0ec4d0dcf05014d4
zcox10 <zach.s.cox@gmail.com>
2025-03-16 20:03:23 +0000

    Roofline

--- project_1/main.py
+++ project_1/main.py
@@ -4,25 +4,13 @@ from models.model_3 import ModelThree
 
 
 def main():
-    # ModelOne: CPU
-    p1 = ModelOne(device_str="cpu")
-    p1.train_model()
+    # # ModelOne: CUDA
+    # p1 = ModelOne(device_str="cuda")
+    # p1.train_model()
 
-    # ModelOne: CUDA
-    p1 = ModelOne(device_str="cuda")
-    p1.train_model()
-
-    # # ModelTwo: CPU
-    # p2 = ModelTwo(device_str="cpu")
-    # p2.train_model()
-
-    # # ModelTwo: CUDA
-    # p2 = ModelTwo(device_str="cuda")
-    # p2.train_model()
-
-    # # ModelThree: CPU
-    # p3 = ModelThree(device_str="cpu")
-    # p3.train_model()
+    # ModelTwo: CUDA
+    p2 = ModelTwo(device_str="cuda")
+    p2.train_model()
 
     # # ModelThree: CUDA
     # p3 = ModelThree(device_str="cuda")
